9fe4f6309ed9d367e686361a534449091df186d5
Frag1212 <antonbarukoff@gmail.com>
2019-11-28 18:23:05 +0300

    Add files via upload

---
+++ AreaLibrary/LibraryUnit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AreaLibrary
+{
+    public static class Area
+    {
+        public static float CalculateArea(float a, float b, float c) //Площадь треугольника
+        {
+            if(a < b)                                   //Делаем a наибольшей стороной для дальнейших проверок
+            {
+                float t = a;
+                a = b;
+                b = t;
+            }
+            if (a < c)
+            {
+                float t = a;
+                a = c;
+                c = t;
+            }
+
+            if (a > b + c)                              //Ни одна сторона не должна быть больше чем сумма двух других. Если одна из сторон отрицательная это условие тоже срабатывает.
+                return -1.0f;
+
+            if(a * a == b * b + c * c)                  //Прямоугольный треугольник
+            {
+                return (b * c) / 2;
+            }
+
+            float S = 0.5f * (a + b + c);
+            float A = S * (S - a) * (S - b) * (S - c);
+
+            return (float)Math.Sqrt(A);
+        }
+        public static float CalculateArea(float r)      //Площадь круга
+        {
+            if (r < 0)
+                return -1.0f;
+
+            return (float)Math.PI * r * r;
+        }
+
+        //Для добавления новой фигуры достаточно сделать новую перегрузку функции CalculateArea
+    }
+}
---
+++ AreaLibraryTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AreaLibrary;
+using System;
+
+namespace AreaLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Тестируем треугольник
+
+            float result = Area.CalculateArea(2.3f, 2.3f, -1.1f);    //Отрицательная сторона
+            float truevalue = -1.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(2.4f, 3.3f, 10.0f);          //Одна сторона больше суммы других
+            truevalue = -1.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(1.2f, 0.0f, 1.2f);           //Нулевая сторона
+            truevalue = 0.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(1.0f, 1.0f, 1.0f);
+            truevalue = 0.433f;
+            float eps = 0.0006f;
+            Assert.IsTrue(Math.Abs(result - truevalue) < eps);
+
+            result = Area.CalculateArea(3.0f, 4.0f, 5.0f);           //Прямоугольный треугольник
+            truevalue = 6.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(1.7f, 3.2f, 2.3f);
+            truevalue = 1.886f;
+            eps = 0.0006f;
+            Assert.IsTrue(Math.Abs(result - truevalue) < eps);
+
+            //Тестируем круг
+
+            result = Area.CalculateArea(-2.3f);                        //Отрицательный радиус
+            truevalue = -1.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(0.0f);                         //Нулевой радиус
+            truevalue = 0.0f;
+            Assert.AreEqual<float>(result, truevalue);
+
+            result = Area.CalculateArea(1.0f);
+            truevalue = 3.142f;
+            eps = 0.0006f;
+            Assert.IsTrue(Math.Abs(result - truevalue) < eps);
+
+            result = Area.CalculateArea(2.3f);
+            truevalue = 16.619f;
+            eps = 0.0006f;
+            Assert.IsTrue(Math.Abs(result - truevalue) < eps);
+        }
+    }
+}
